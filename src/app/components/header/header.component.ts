0b16154610ee37ebb7e2d9c99cafab7c2d47c31d
your_username <chandabhishek45@gmail.com>
2025-01-30 18:30:44 +0545

    added btn models product-list page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   template: `
-    <div class="bg-state-100 px-4 py-3 shadow-md ">
-      <span>{{ title() }}</span>
+    <div class="bg-state-100 px-4 py-3 shadow-md flex justify-between items-center">
+      <span class="text-xl">{{ title() }}</span>
+      <app-primary-button [label]="cart()" (btnClicked)="showBtnClicked()"/>
     </div>
   `,
   styles: ``
 })
 export class HeaderComponent {
   title = signal<string>('My Store')
+
+  cart = signal('Cart');
+
+  showBtnClicked() {
+    console.log('btn clicked');
+    
+  }
 }
